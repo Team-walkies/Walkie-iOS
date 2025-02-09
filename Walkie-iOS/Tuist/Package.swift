4068a817818fa0708d9c03f064c947c88f4d1530
ahra <ahravelop@gmail.com>
2025-02-09 21:58:25 +0900

    #7 fix: 로티 spm url로 재변경

--- Walkie-iOS/Tuist/Package.swift
+++ Walkie-iOS/Tuist/Package.swift
@@ -15,7 +15,7 @@ import PackageDescription
 let package = Package(
     name: "Walkie-iOS",
     dependencies: [
-        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.5.1")
+        .package(url: "https://github.com/airbnb/lottie-spm.git", from: "4.5.1")
     ],
     targets: [
         .target(
