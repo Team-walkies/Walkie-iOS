30e6abbaf00ca2f1eb1b92d2690e3d5445bdb415
woongaaaa <woonge0930@naver.com>
2025-02-13 00:06:14 +0900

    #24 feat: alignTo() 함수 구현

--- Walkie-iOS/Walkie-iOS/Sources/Extension/View+.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Extension/View+.swift
@@ -21,4 +21,33 @@ extension View {
             .padding(.vertical, fontSpacing)
             .lineSpacing(fontSpacing * 2)
     }
+    
+    func alignTo(_ alignment: Alignment) -> some View {
+        modifier(AlignmentModifier(alignment: alignment))
+    }
+}
+
+// MARK: - AlignmentModifier
+struct AlignmentModifier: ViewModifier {
+    let alignment: Alignment
+
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            if alignment == .bottom || alignment == .bottomLeading || alignment == .bottomTrailing {
+                Spacer(minLength: 0)
+            }
+            HStack(spacing: 0) {
+                if alignment == .trailing || alignment == .bottomTrailing || alignment == .topTrailing {
+                    Spacer(minLength: 0)
+                }
+                content
+                if alignment == .leading || alignment == .bottomLeading || alignment == .topLeading {
+                    Spacer(minLength: 0)
+                }
+            }
+            if alignment == .top || alignment == .topLeading || alignment == .topTrailing {
+                Spacer(minLength: 0)
+            }
+        }
+    }
 }
