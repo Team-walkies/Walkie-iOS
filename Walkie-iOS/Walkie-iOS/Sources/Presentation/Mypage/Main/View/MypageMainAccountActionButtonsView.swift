e08110b9d4a83b751f291c2444fe370018187473
woongaaaa <woonge0930@naver.com>
2025-03-01 22:42:10 +0900

    #28 fix: 뷰 분리

---
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Mypage/Main/View/MypageMainAccountActionButtonsView.swift
@@ -0,0 +1,38 @@
+//
+//  MypageMainAccountActionButtonsView.swift
+//  Walkie-iOS
+//
+//  Created by 황채웅 on 3/1/25.
+//
+
+import SwiftUI
+
+struct MypageMainAccountActionButtonsView: View {
+    
+    @ObservedObject var viewModel: MypageMainViewModel
+
+    var body: some View {
+        HStack(spacing: 12) {
+            Button {
+                // 로그아웃
+            } label: {
+                Text("로그아웃")
+                    .font(.B2)
+                    .foregroundStyle(.gray400)
+            }
+            Rectangle()
+                .frame(width: 1, height: 16)
+                .foregroundStyle(.gray300)
+            NavigationLink(
+                destination: MypageWithdrawView(viewModel: viewModel)
+                    .navigationBarBackButtonHidden(),
+                label: {
+                Text("탈퇴하기")
+                    .font(.B2)
+                    .foregroundStyle(.gray400)
+            }
+            )
+        }
+        .frame(alignment: .center)
+    }
+}
--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Mypage/Main/View/MypageMainView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Mypage/Main/View/MypageMainView.swift
@@ -19,7 +19,7 @@ struct MypageMainView: View {
                     hasAlarm: mypageMainState.hasAlarm)
                 ScrollView(.vertical, showsIndicators: false) {
                     VStack(spacing: 0) {
-                        ProfileSectionView(mypageMainState: mypageMainState)
+                        MypageMainProfileSectionView(mypageMainState: mypageMainState)
                             .padding(.bottom, 20)
                         
                         MypageMainSettingSectionView(viewModel: viewModel)
@@ -28,10 +28,10 @@ struct MypageMainView: View {
                         MypageMainServiceSectionView(viewModel: viewModel)
                             .padding(.bottom, 8)
                         
-                        FeedbackButtonView()
+                        MypageMainFeedbackButtonView()
                             .padding(.bottom, 12)
                         
-                        AccountActionButtonsView(viewModel: viewModel)
+                        MypageMainAccountActionButtonsView(viewModel: viewModel)
                     }
                     .frame(alignment: .top)
                     .padding(.horizontal, 16)
@@ -45,100 +45,6 @@ struct MypageMainView: View {
         .onAppear {
             viewModel.action(.mypageMainWillAppear)
         }
-    }
-}
-
-private struct ProfileSectionView: View {
-    
-    let mypageMainState: MypageMainViewModel.MypageMainState
-    
-    var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack(alignment: .center, spacing: 0) {
-                Text(mypageMainState.nickname)
-                    .font(.H2)
-                    .foregroundStyle(.gray700)
-                Text("님")
-                    .font(.H2)
-                    .foregroundStyle(.gray500)
-                    .padding(.trailing, 8)
-                Text(mypageMainState.userTier)
-                    .font(.B2)
-                    .foregroundStyle(.blue400)
-                    .padding(.horizontal, 8)
-                    .frame(height: 28)
-                    .background(.blue50)
-                    .cornerRadius(8)
-                Spacer()
-            }
-            
-            HighlightTextAttribute(
-                text: "지금까지 \(mypageMainState.spotCount)개의 스팟을 탐험했어요",
-                textColor: .gray500,
-                font: .B1,
-                highlightText: "\(mypageMainState.spotCount)",
-                highlightColor: .blue400,
-                highlightFont: .H5)
-        }
-    }
-}
-
-struct FeedbackButtonView: View {
-    var body: some View {
-        NavigationLink(destination: EmptyView().navigationBarBackButtonHidden()) {
-            HStack(spacing: 0) {
-                Image(.icMyFeedback)
-                    .frame(width: 36, height: 36)
-                    .padding(.trailing, 8)
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("앱에 대한 의견을 남겨주세요!")
-                        .font(.B2)
-                        .foregroundStyle(.gray700)
-                    Text("더 나은 서비스를 위해 노력할게요")
-                        .font(.C1)
-                        .foregroundStyle(.gray500)
-                }
-                Spacer()
-                Image(.icChevronRight)
-                    .frame(width: 28, height: 28)
-                    .foregroundColor(.gray300)
-            }
-            .padding(16)
-        }
-        .buttonStyle(PlainButtonStyle())
-        .frame(height: 72)
-        .background(.gray100)
-        .cornerRadius(12)
-    }
-}
-
-private struct AccountActionButtonsView: View {
-    
-    @ObservedObject var viewModel: MypageMainViewModel
-
-    var body: some View {
-        HStack(spacing: 12) {
-            Button {
-                // 로그아웃
-            } label: {
-                Text("로그아웃")
-                    .font(.B2)
-                    .foregroundStyle(.gray400)
-            }
-            Rectangle()
-                .frame(width: 1, height: 16)
-                .foregroundStyle(.gray300)
-            NavigationLink(
-                destination: MypageWithdrawView(viewModel: viewModel)
-                    .navigationBarBackButtonHidden(),
-                label: {
-                Text("탈퇴하기")
-                    .font(.B2)
-                    .foregroundStyle(.gray400)
-            }
-            )
-        }
-        .frame(alignment: .center)
     }
 }
 
