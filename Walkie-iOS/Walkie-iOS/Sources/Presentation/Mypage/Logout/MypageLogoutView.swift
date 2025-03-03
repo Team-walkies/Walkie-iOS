8fc88e5d5f41dfe526929fcd41f2a5cf39480d0d
woongaaaa <woonge0930@naver.com>
2025-03-03 16:20:12 +0900

    #28 feat: 로그아웃 모달 dim 영역 나가기 로직 구현

--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Mypage/Logout/MypageLogoutView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Mypage/Logout/MypageLogoutView.swift
@@ -8,32 +8,36 @@
 import SwiftUI
 
 struct MypageLogoutView: View {
-    
     @ObservedObject var viewModel: MypageMainViewModel
     
     var body: some View {
-        if viewModel.logoutViewState.isPresented {
-            ZStack {
+        ZStack {
+            if viewModel.logoutViewState.isPresented {
                 Color(white: 0, opacity: 0.6)
+                    .ignoresSafeArea()
+                    .transition(.opacity)
+                    .onTapGesture {
+                        viewModel.logoutViewState.isPresented = false
+                    }
                 Modal(
                     title: "로그아웃",
                     content: "앱에서 로그아웃할까요?",
                     style: .error,
                     button: .twobutton,
                     cancelButtonAction: {
-                        viewModel.logoutViewState.isPresented.toggle()
+                        viewModel.logoutViewState.isPresented = false
                     },
                     checkButtonAction: {
                         viewModel.action(.logout)
                     },
                     checkButtonTitle: "로그아웃",
                     cancelButtonTitle: "뒤로가기")
+                .transition(.opacity)
             }
-            .ignoresSafeArea(.all)
         }
+        .animation(.easeInOut(duration: 0.25), value: viewModel.logoutViewState.isPresented)
     }
 }
-
 #Preview {
     MypageLogoutView(viewModel: MypageMainViewModel())
 }
