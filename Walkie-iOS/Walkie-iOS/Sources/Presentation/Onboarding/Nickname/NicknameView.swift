b55007be687c6c09062cb526bd8cec29f28a2d88
ahra <ahravelop@gmail.com>
2025-03-04 14:45:42 +0900

    #40 feat: 온보딩 완료 뷰 ui 구현

--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Nickname/NicknameView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Nickname/NicknameView.swift
@@ -12,43 +12,49 @@ struct NicknameView: View {
     @State private var userInput: String = ""
     @State private var isButtonEnabled: Bool = false
     @State private var inputState: InputState = .default
+    @State private var isNavigating: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            NavigationBar(
-                rightButtonTitle: "완료",
-                showRightButton: true,
-                rightButtonEnabled: isButtonEnabled,
-                rightButtonAction: {
-                    print("rightbuttontapped")
+        NavigationStack {
+            VStack(alignment: .leading) {
+                NavigationBar(
+                    rightButtonTitle: "완료",
+                    showRightButton: true,
+                    rightButtonEnabled: isButtonEnabled,
+                    rightButtonAction: {
+                        print("rightbuttontapped")
+                        isNavigating = true
+                    }
+                )
+                
+                Text("워키에서 사용할\n닉네임을 지어주세요")
+                    .font(.H2)
+                    .foregroundColor(.gray700)
+                    .padding(.top, 24)
+                    .padding(.leading, 16)
+                
+                InputView(
+                    limitation: 20,
+                    placeholderText: "닉네임",
+                    onlyText: true,
+                    input: $userInput,
+                    inputState: $inputState
+                )
+                .padding(.top, 38)
+                .padding(.horizontal, 16)
+                .onChange(of: inputState) { _, newState in
+                    isButtonEnabled = newState == .focus
                 }
-            )
-            
-            Text("워키에서 사용할\n닉네임을 지어주세요")
-                .font(.H2)
-                .foregroundColor(.gray700)
-                .padding(.top, 24)
-                .padding(.leading, 16)
-            
-            InputView(
-                limitation: 20,
-                placeholderText: "닉네임",
-                onlyText: true,
-                input: $userInput,
-                inputState: $inputState
-            )
-            .padding(.top, 38)
-            .padding(.horizontal, 16)
-            .onChange(of: inputState) { _, newState in
-                isButtonEnabled = newState == .focus
+                
+                Spacer()
             }
-            
-            Spacer()
+            .alignTo(.leading)
+            .toolbar(.hidden, for: .navigationBar)
         }
-        .alignTo(.leading)
-        .toolbar(.hidden, for: .navigationBar)
+        .navigationDestination(isPresented: $isNavigating) {
+            OnboardingCompleteView()
+        }
     }
-    
 }
 
 struct NicknameView_Previews: PreviewProvider {
