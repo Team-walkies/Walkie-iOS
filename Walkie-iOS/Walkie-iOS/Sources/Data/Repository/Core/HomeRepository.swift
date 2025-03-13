ff084e19d8a4222cc42bcf13f97f104737c7454c
ahra <ahravelop@gmail.com>
2025-03-13 23:40:06 +0900

    #47 feat: HomeRepository 구현
    - HomeService 주입

---
+++ Walkie-iOS/Walkie-iOS/Sources/Data/Repository/Core/HomeRepository.swift
@@ -0,0 +1,13 @@
+//
+//  HomeRepository.swift
+//  Walkie-iOS
+//
+//  Created by ahra on 3/12/25.
+//
+
+import Combine
+
+protocol HomeRepository {
+    
+    func getEggCount() -> AnyPublisher<EggCountDto, Error>
+}
