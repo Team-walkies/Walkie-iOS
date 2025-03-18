eb518e44c752a8428ef3a789cf263756b2110fc0
woongaaaa <woonge0930@naver.com>
2025-03-19 02:11:02 +0900

    #49 feat: 알 안내 페이지 UI 구현

--- Walkie-iOS/Walkie-iOS/Sources/Const/EggLiterals.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Const/EggLiterals.swift
@@ -7,11 +7,11 @@
 
 import SwiftUI
 
-enum EggLiterals: String {
-    case normal = "일반",
-         rare = "희귀",
-         epic = "에픽",
-         legendary = "전설"
+enum EggLiterals: String, CaseIterable, Hashable {
+    case normal = "일반"
+    case rare = "희귀"
+    case epic = "에픽"
+    case legendary = "전설"
     
     var fontColor: Color {
         switch self {
@@ -52,4 +52,58 @@ enum EggLiterals: String {
         }
     }
     
+    var obtainRate: Int {
+        switch self {
+        case .normal:
+            65
+        case .rare:
+            20
+        case .epic:
+            12
+        case .legendary:
+            3
+        }
+    }
+    
+    var characterObtainRate: [EggLiterals: Int] {
+        switch self {
+        case .normal: [
+            .normal: 80,
+            .rare: 15,
+            .epic: 4,
+            .legendary: 1
+        ]
+        case .rare: [
+            .normal: 50,
+            .rare: 35,
+            .epic: 10,
+            .legendary: 5
+        ]
+        case .epic: [
+            .normal: 20,
+            .rare: 40,
+            .epic: 30,
+            .legendary: 10
+        ]
+        case .legendary: [
+            .normal: 5,
+            .rare: 25,
+            .epic: 40,
+            .legendary: 30
+        ]
+        }
+    }
+    
+    var eggInformationText: String {
+        switch self {
+        case .normal:
+            "자주 얻을 수 있는 알"
+        case .rare:
+            "평소보다 더 희귀한 알"
+        case .epic:
+            "보기 드문 비범한 알"
+        case .legendary:
+            "전설로만 듣던 알"
+        }
+    }
 }
---
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Egg/EggGuideView.swift
@@ -0,0 +1,132 @@
+//
+//  EggGuideView.swift
+//  Walkie-iOS
+//
+//  Created by 황채웅 on 3/19/25.
+//
+
+import SwiftUI
+
+struct EggGuideView: View {
+    var body: some View {
+        NavigationBar(showBackButton: true)
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Text("알 얻을 확률")
+                    .font(.H2)
+                    .foregroundStyle(.gray700)
+                    .padding(.top, 12)
+                    .padding(.bottom, 4)
+                Text("스팟 탐험 시 알을 얻을 확률이에요")
+                    .font(.B2)
+                    .foregroundStyle(.gray500)
+                    .padding(.bottom, 12)
+                ForEach(EggLiterals.allCases, id: \.self) { eggType in
+                    EggGuideItemView(eggType: eggType)
+                }
+                Text("캐릭터 얻을 확률")
+                    .font(.H2)
+                    .foregroundStyle(.gray700)
+                    .padding(.top, 40)
+                    .padding(.bottom, 4)
+                Text("얻기 어려운 알일수록 희귀한 캐릭터가 부화해요")
+                    .font(.B2)
+                    .foregroundStyle(.gray500)
+                    .padding(.bottom, 20)
+                ForEach(EggLiterals.allCases, id: \.self) { eggType in
+                    CharacterGuideItemView(eggType: eggType)
+                    if eggType != EggLiterals.allCases.last! {
+                        Rectangle()
+                            .frame(height: 2)
+                            .foregroundStyle(.gray50)
+                    }
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(.horizontal, 16)
+        }
+    }
+}
+
+// 알 얻을 확률
+private struct EggGuideItemView: View {
+    let eggType: EggLiterals
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Image(eggType.eggImage)
+                .resizable()
+                .frame(width: 60, height: 60)
+                .padding(.leading, 8)
+                .padding(.trailing, 4)
+            VStack(alignment: .leading, spacing: 4) {
+                Text("\(eggType.rawValue) 알")
+                    .font(.B1)
+                    .foregroundStyle(eggType.fontColor)
+                Text(eggType.eggInformationText)
+                    .font(.B2)
+                    .foregroundStyle(.gray500)
+            }
+            Spacer()
+            Text("\(eggType.obtainRate)%")
+                .font(.H3)
+                .foregroundStyle(.gray700)
+                .padding(.trailing, 20)
+        }
+        .frame(height: 76)
+        .background(.gray50)
+        .cornerRadius(12)
+        .padding(.top, 8)
+    }
+}
+
+// 캐릭터 얻을 확률
+private struct CharacterGuideItemView: View {
+    let eggType: EggLiterals
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            VStack(alignment: .center, spacing: 4) {
+                Image(eggType.eggImage)
+                    .resizable()
+                    .frame(width: 120, height: 120)
+                    .padding(.leading, 8)
+                    .padding(.trailing, 4)
+                Text("\(eggType.rawValue) 알")
+                    .font(.B1)
+                    .foregroundStyle(eggType.fontColor)
+            }
+            Spacer()
+            VStack(alignment: .leading, spacing: 16) {
+                ForEach(EggLiterals.allCases, id: \.rawValue) { (characterType: EggLiterals) in
+                    CharacterProbabilityView(eggType: eggType, characterType: characterType)
+                }
+            }
+        }
+        .padding(.leading, 20)
+        .padding(.trailing, 40)
+        .padding(.vertical, 22)
+        .frame(height: 188)
+    }
+}
+
+// 캐릭터 등급 별 확률
+private struct CharacterProbabilityView: View {
+    let eggType: EggLiterals
+    let characterType: EggLiterals
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 8) {
+            Text("\(characterType.rawValue) 캐릭터")
+                .font(.B1)
+                .foregroundStyle(.gray500)
+            Text("\(eggType.characterObtainRate[characterType] ?? 0)%")
+                .font(.H5)
+                .foregroundStyle(.gray700)
+        }
+    }
+}
+
+#Preview {
+    EggGuideView()
+}
