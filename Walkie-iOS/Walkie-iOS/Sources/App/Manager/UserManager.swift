ba0c2421edb2bae75cc9121370dd96fa63999eee
woongaaaa <woonge0930@naver.com>
2025-05-03 20:09:06 +0900

    #77 feat: UserManager 알 부화 관련 프로퍼티 추가

--- Walkie-iOS/Walkie-iOS/Sources/App/Manager/UserManager.swift
+++ Walkie-iOS/Walkie-iOS/Sources/App/Manager/UserManager.swift
@@ -15,6 +15,12 @@ final class UserManager {
     
     @UserDefaultsWrapper<String>(key: "nickname") private(set) var nickname
     
+    @UserDefaultsWrapper<Bool>(key: "willHatch") private(set) var willHatch
+    @UserDefaultsWrapper<EggType>(key: "eggType") private(set) var eggType
+    @UserDefaultsWrapper<CharacterType>(key: "characterType") private(set) var characterType
+    @UserDefaultsWrapper<JellyfishType>(key: "jellyfishType") private(set) var jellyfishType
+    @UserDefaultsWrapper<DinoType>(key: "dinoType") private(set) var dinoType
+    
     private init() {}
 }
 
@@ -22,12 +28,36 @@ extension UserManager {
     
     var hasUserToken: Bool { return TokenKeychainManager.shared.hasToken() }
     var getUserNickname: String { return self.nickname ?? "" }
+    var getEggType: EggType { return self.eggType ?? .normal }
+    var getCharacterType: CharacterType { return self.characterType ?? .dino }
+    var getJellyfishType: JellyfishType { return self.jellyfishType ?? .defaultJellyfish }
+    var getDinoType: DinoType { return self.dinoType ?? .defaultDino }
 }
 
 extension UserManager {
     
     func setUserNickname(_ nickname: String) {
         self.nickname = nickname
+    }
+    
+    func updateHatchState(_ willHatch: Bool) {
+        self.willHatch = willHatch
+    }
+    
+    func setEggType(_ eggType: EggType) {
+        self.eggType = eggType
+    }
+    
+    func setCharacterType(_ characterType: CharacterType) {
+        self.characterType = characterType
+    }
+    
+    func setJellyfishType(_ jellyfishType: JellyfishType) {
+        self.jellyfishType = jellyfishType
+    }
+    
+    func setDinoType(_ dinoType: DinoType) {
+        self.dinoType = dinoType
     }
     
     func withdraw() {
