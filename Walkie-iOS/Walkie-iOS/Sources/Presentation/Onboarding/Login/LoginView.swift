bc51e976f2170614a17259e3cb81ea4c406957c7
arako <ahravelop@gmail.com>
2025-06-02 15:14:13 +0900

    #152 feat: 온보딩완료뷰 ui 구현

--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Login/LoginView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Login/LoginView.swift
@@ -12,6 +12,7 @@ import WalkieCommon
 struct LoginView: View {
     
     @Environment(\.screenWidth) var screenWidth
+    @Environment(\.screenHeight) var screenHeight
     @State private var isNavigating: Bool = false
     
     private let onboardingPage = OnboardingPageStruct.makeOnboardingPage()
@@ -19,77 +20,79 @@ struct LoginView: View {
     @StateObject var loginViewModel: LoginViewModel
     
     var body: some View {
-        NavigationStack(path: $appCoordinator.path) {
-            VStack(alignment: .center) {
-                TabView {
-                    ForEach(onboardingPage, id: \.title) { item in
-                        VStack(alignment: .center, spacing: 32) {
-                            Image(item.image)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 343, height: 343)
+        VStack(alignment: .center) {
+            TabView {
+                ForEach(onboardingPage, id: \.title) { item in
+                    VStack(
+                        alignment: .center,
+                        spacing: 32
+                    ) {
+                        let imageSize = screenWidth - 32
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: imageSize, height: imageSize)
+                        
+                        VStack(spacing: 12) {
+                            Text(item.title)
+                                .font(.H3)
+                                .foregroundColor(WalkieCommonAsset.gray700.swiftUIColor)
                             
-                            VStack(spacing: 12) {
-                                Text(item.title)
-                                    .font(.H3)
-                                    .foregroundColor(WalkieCommonAsset.gray700.swiftUIColor)
-                                
-                                Text(item.subTitle)
-                                    .font(.B1)
-                                    .foregroundColor(WalkieCommonAsset.gray500.swiftUIColor)
-                                    .multilineTextAlignment(.center)
-                            }
-                            Spacer()
+                            Text(item.subTitle)
+                                .font(.B1)
+                                .foregroundColor(WalkieCommonAsset.gray500.swiftUIColor)
+                                .multilineTextAlignment(.center)
                         }
                     }
                 }
-                .onAppear { setIndicator() }
-                .tabViewStyle(.page(indexDisplayMode: .always))
-                
-                Spacer()
-                
-                Button(action: {
-                    loginViewModel.action(.tapKakaoLogin)
-                    isNavigating = true
-                }, label: {
-                    HStack(spacing: 8) {
-                        Image(.icKakao)
-                            .renderingMode(.original)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
-                        
-                        Text("카카오 계정으로 계속하기")
-                            .font(.B1)
-                            .foregroundColor(.black)
-                    }
-                })
-                .frame(width: screenWidth - 32, height: 54)
-                .background(WalkieCommonAsset.yellow100.swiftUIColor)
-                .cornerRadius(12, corners: .allCorners)
-                .padding(.top, 41)
-                
-                Button(action: {
-                    loginViewModel.action(.tapAppleLogin)
-                    isNavigating = true
-                }, label: {
-                    HStack(spacing: 8) {
-                        Image(.icApple)
-                            .renderingMode(.original)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 24, height: 24)
-                        
-                        Text("Apple로 시작하기")
-                            .font(.B1)
-                            .foregroundColor(.white)
-                    }
-                })
-                .frame(width: screenWidth - 32, height: 54)
-                .background(.black)
-                .cornerRadius(12, corners: .allCorners)
-                .padding(.bottom, 4)
             }
+            .onAppear { setIndicator() }
+            .tabViewStyle(.page(indexDisplayMode: .always))
+            .frame(maxHeight: .infinity)
+            
+            Spacer()
+            
+            Button(action: {
+                loginViewModel.action(.tapKakaoLogin)
+                isNavigating = true
+            }, label: {
+                HStack(spacing: 8) {
+                    Image(.icKakao)
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                    
+                    Text("카카오 계정으로 계속하기")
+                        .font(.B1)
+                        .foregroundColor(.black)
+                }
+            })
+            .frame(width: screenWidth - 32, height: 54)
+            .background(WalkieCommonAsset.yellow100.swiftUIColor)
+            .cornerRadius(12, corners: .allCorners)
+            .padding(.top, 41)
+            
+            Button(action: {
+                loginViewModel.action(.tapAppleLogin)
+                isNavigating = true
+            }, label: {
+                HStack(spacing: 8) {
+                    Image(.icApple)
+                        .renderingMode(.original)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                    
+                    Text("Apple로 시작하기")
+                        .font(.B1)
+                        .foregroundColor(.white)
+                }
+            })
+            .frame(width: screenWidth - 32, height: 54)
+            .background(.black)
+            .cornerRadius(12, corners: .allCorners)
+            .padding(.bottom, 4)
         }
         .onChange(of: loginViewModel.state) { _, newState in
             switch newState {
--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Nickname/NicknameView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Onboarding/Nickname/NicknameView.swift
@@ -20,69 +20,67 @@ struct NicknameView: View {
     @EnvironmentObject private var appCoordinator: AppCoordinator
     
     var body: some View {
-        NavigationStack(path: $appCoordinator.path) {
-            VStack(alignment: .leading) {
-                NavigationBar(
-                    rightButtonTitle: "완료",
-                    showRightButton: true,
-                    rightButtonEnabled: isButtonEnabled,
-                    rightButtonShowsEnabledColor: true,
-                    rightButtonAction: {
-                        let info = LoginUserInfo(
-                            provider: appCoordinator.loginInfo.provider,
-                            socialToken: appCoordinator.loginInfo.socialToken,
-                            username: userInput
-                        )
-                        signupViewModel.action(.tapSignup(info: info))
-                        UserManager.shared.setUserNickname(userInput)
-                    }
-                )
-                
-                Text("워키에서 사용할\n닉네임을 지어주세요")
-                    .font(.H2)
-                    .foregroundColor(WalkieCommonAsset.gray700.swiftUIColor)
-                    .padding(.top, 24)
-                    .padding(.leading, 16)
-                
-                InputView(
-                    limitation: 10,
-                    placeholderText: appCoordinator.loginInfo.username == ""
-                    ? "닉네임 입력"
-                    : appCoordinator.loginInfo.username,
-                    onlyText: true,
-                    input: $userInput,
-                    inputState: $inputState
-                )
-                .focused($focused)
-                .padding(.top, 38)
-                .padding(.horizontal, 16)
-                .onChange(of: inputState) { _, newState in
-                    isButtonEnabled = newState == .focus
+        VStack(alignment: .leading) {
+            NavigationBar(
+                rightButtonTitle: "완료",
+                showRightButton: true,
+                rightButtonEnabled: isButtonEnabled,
+                rightButtonShowsEnabledColor: true,
+                rightButtonAction: {
+                    let info = LoginUserInfo(
+                        provider: appCoordinator.loginInfo.provider,
+                        socialToken: appCoordinator.loginInfo.socialToken,
+                        username: userInput
+                    )
+                    signupViewModel.action(.tapSignup(info: info))
+                    UserManager.shared.setUserNickname(userInput)
                 }
-                
-                Spacer()
+            )
+            
+            Text("워키에서 사용할\n닉네임을 지어주세요")
+                .font(.H2)
+                .foregroundColor(WalkieCommonAsset.gray700.swiftUIColor)
+                .padding(.top, 24)
+                .padding(.leading, 16)
+            
+            InputView(
+                limitation: 10,
+                placeholderText: appCoordinator.loginInfo.username == ""
+                ? "닉네임 입력"
+                : appCoordinator.loginInfo.username,
+                onlyText: true,
+                input: $userInput,
+                inputState: $inputState
+            )
+            .focused($focused)
+            .padding(.top, 38)
+            .padding(.horizontal, 16)
+            .onChange(of: inputState) { _, newState in
+                isButtonEnabled = newState == .focus
             }
-            .alignTo(.leading)
-            .toolbar(.hidden, for: .navigationBar)
-            .contentShape(Rectangle())
-            .onAppear {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                    self.focused = true
-                }
+            
+            Spacer()
+        }
+        .alignTo(.leading)
+        .toolbar(.hidden, for: .navigationBar)
+        .contentShape(Rectangle())
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.focused = true
             }
-            .onTapGesture {
-                hideKeyboard()
-            }
-            .gesture(DragGesture().onChanged { _ in
-                hideKeyboard()
-            })
-            .onChange(of: signupViewModel.state) { _, newState in
-                switch newState {
-                case .loaded:
-                    appCoordinator.currentScene = .complete
-                default:
-                    break
-                }
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+        .gesture(DragGesture().onChanged { _ in
+            hideKeyboard()
+        })
+        .onChange(of: signupViewModel.state) { _, newState in
+            switch newState {
+            case .loaded:
+                appCoordinator.currentScene = .complete
+            default:
+                break
             }
         }
     }
