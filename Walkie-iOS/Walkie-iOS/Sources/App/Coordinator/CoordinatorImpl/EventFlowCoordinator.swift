c2a45a938d7f9b7236c5e7fa4eaeaa026d56c3f1
arako <ahravelop@gmail.com>
2025-07-23 13:06:15 +0900

    #203 feat: EventFlowCoordinator 구현

---
+++ Walkie-iOS/Walkie-iOS/Sources/App/Coordinator/CoordinatorImpl/EventFlowCoordinator.swift
@@ -0,0 +1,68 @@
+//
+//  EventFlowCoordinator.swift
+//  Walkie-iOS
+//
+//  Created by 고아라 on 7/21/25.
+//
+
+import Foundation
+import Combine
+
+final class EventFlowCoordinator: ObservableObject {
+    
+    private let remoteConfigManager: RemoteConfigManaging
+    private let getEventEggUseCase: GetEventEggUseCase
+    
+    @Published private(set) var eventEggEntity: EventEggEntity?
+
+    private var cancellables = Set<AnyCancellable>()
+
+    init(
+        remoteConfigManager: RemoteConfigManaging = RemoteConfigManager.shared,
+        getEventEggUseCase: GetEventEggUseCase
+    ) {
+        self.remoteConfigManager = remoteConfigManager
+        self.getEventEggUseCase  = getEventEggUseCase
+    }
+
+    func checkEvent(completion: (() -> Void)? = nil) {
+        Task {
+            do {
+                try await remoteConfigManager.fetchAndActivate()
+                guard remoteConfigManager.boolValue(for: .eggEventEnabled) else { return }
+
+                let now = Date()
+                let calendar = Calendar.current
+                let oneDayAgo = calendar.date(
+                    byAdding: .day, value: -1, to: now
+                ) ?? now
+                
+                let lastDate = UserManager.shared.getLastVisitedDate
+                ?? oneDayAgo
+                let daysDiff = calendar.dateComponents(
+                    [.day],
+                    from: calendar.startOfDay(for: lastDate),
+                    to: calendar.startOfDay(for: now)
+                ).day ?? 0
+                
+                guard daysDiff >= 1 else { return }
+                
+                getEventEggUseCase.getEventEgg()
+                    .walkieSink(
+                        with: self,
+                        receiveValue: { _, entity in
+                            self.eventEggEntity = entity.canReceive ? entity : nil
+                            UserManager.shared.setLastVisitedDate(now)
+                            completion?()
+                        },
+                        receiveFailure: { _, _ in
+                            self.eventEggEntity = nil
+                            completion?()
+                        }
+                    )
+                    .store(in: &cancellables)
+            } catch {
+            }
+        }
+    }
+}
