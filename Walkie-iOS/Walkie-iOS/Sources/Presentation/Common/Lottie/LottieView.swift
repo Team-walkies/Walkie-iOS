8c1ad879e020439b299a99744b9cc4842c2a6412
arako <ahravelop@gmail.com>
2025-08-04 13:50:33 +0900

    #216 fix: LottieView에 isLoop 플래그 추가

--- Walkie-iOS/Walkie-iOS/Sources/Presentation/Common/Lottie/LottieView.swift
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Common/Lottie/LottieView.swift
@@ -9,39 +9,23 @@ import SwiftUI
 
 import Lottie
 
-enum WalkieLottie {
-    case confetti
-    case eggBlue
-    case eggGreen
-    case eggPurple
-    case eggYellow
-    
-    var filename: String {
-        switch self {
-        case .confetti:
-            return "walkie_Confetti"
-        case .eggBlue:
-            return "walkie_EggBlue"
-        case .eggGreen:
-            return "walkie_EggGreen"
-        case .eggPurple:
-            return "walkie_EggPurple"
-        case .eggYellow:
-            return "walkie_EggYellow"
-        }
-    }
-}
-
 struct WalkieLottieView: View {
     let lottie: WalkieLottie
     let isPlaying: Bool
+    var isLoop: Bool = false
 
     var body: some View {
-        LottieView(animation: .named(lottie.filename))
-            .playbackMode(isPlaying ? .playing(.fromProgress(0, toProgress: 1, loopMode: .playOnce)) : .paused)
-            .configure { lottieAnimationView in
-                lottieAnimationView.contentMode = .scaleAspectFit
-                lottieAnimationView.shouldRasterizeWhenIdle = true
-            }
+        LottieView(
+            animation: .named(lottie.filename)
+        )
+        .playbackMode(
+            isPlaying
+            ? .playing(.fromProgress(0, toProgress: 1, loopMode: isLoop ? .loop : .playOnce))
+            : .paused
+        )
+        .configure { lottieAnimationView in
+            lottieAnimationView.contentMode = .scaleAspectFit
+            lottieAnimationView.shouldRasterizeWhenIdle = true
+        }
     }
 }
