88b9ee7477072e3972ab2b595973e309fa3bb006
woongaaaa <woonge0930@naver.com>
2025-08-05 04:53:57 +0900

    #217 feat: 건강 권한 요청 뷰 및 뷰모델 구현

---
+++ Walkie-iOS/Walkie-iOS/Sources/Presentation/Main/HealthCare/HealthCarePermissionGuide/HealthCarePermissionGuideViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  HealthCarePermissionGuideViewModel.swift
+//  Walkie-iOS
+//
+//  Created by 황채웅 on 8/5/25.
+//
+
+import Foundation
+
+final class HealthCarePermissionGuideViewModel: ViewModelable {
+    
+    struct State {
+    }
+    
+    enum Action {
+        case checkedButtonTapped
+        case returnFromPermissionSetting
+    }
+    
+    var coordinator: Coordinator
+    var state: State = State()
+    
+    public init(coordinator: Coordinator) {
+        self.coordinator = coordinator
+    }
+    
+    func action(_ action: Action) {
+        switch action {
+        case .checkedButtonTapped:
+            HealthKitManager.shared.requestHealthKitAuthorization()
+        case .returnFromPermissionSetting:
+            switch HealthKitManager.shared.checkAuthorizationStatus() {
+            case .authorized:
+                coordinator.push(AppScene.healthcare)
+            default:
+                break
+            }
+        }
+    }
+}
