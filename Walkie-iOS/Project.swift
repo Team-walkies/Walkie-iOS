5498ebef90f4e3847b99431e541fb2a6f6470208
woongaaaa <woonge0930@naver.com>
2025-10-22 17:35:35 +0900

    #244 hotfix: iOS 26 리퀴드글래스 임시 조치

--- Walkie-iOS/Project.swift
+++ Walkie-iOS/Project.swift
@@ -88,7 +88,8 @@ let project = Project(
                         "fetch",
                         "processing",
                         "location"
-                    ]
+                    ],
+                    "UIDesignRequiresCompatibility": true
                 ]
             ),
             sources: ["Walkie-iOS/Sources/**"],
